a030c2bdfbfe4c611fa552142ca425020b8b7b76
camidev234 <camilo222andresace@gmail.com>
2024-02-16 18:54:59 -0500

    Edit experiencie styles

--- resources/views/person_exp/edit.blade.php
+++ resources/views/person_exp/edit.blade.php
@@ -3,39 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/createEducation.css')}}">
+    <link rel="stylesheet" href="{{asset('css/editExperiencie.css')}}">
     <title>Actualizar</title>
 </head>
 <body>
-    <header class="header">
-        @extends('layout.header')
-    </header>
-    @section('content')
-    <main class="container">
-        <form action="{{route('exp.update', ['person_experience' => $experience->id])}}" method="post">
-            @csrf
-            @method('PATCH')
-            <label for="company_experience">Empresa: </label><br>
-            <input type="text" placeholder="Empresa" name="company_experience" value="{{old('company_experience')}} {{$experience->company_experience}}"><br>
-            @error('company_experience')
-                <span style="color:red;">{{$message}}</span>
-                <br>
-            @enderror
-            <label for="months_experience">Meses de Experiencia: </label><br>
-            <input type="text" name="months_experience" placeholder="meses de experiencia" value="{{old('months_experience')}} {{$experience->months_experience}}">
-            @error('months_experience')
-                <span style="color: red;">{{$message}}</span><br>
-            @enderror
-            <label for="">Funciones Desempeñadas:</label><br>
-
-            <textarea type="text" name="functions" placeholder="Funciones" rows="15">{{old('functions')}} {{$experience->functions}}</textarea><br>
-            @error('functions')
-                <span style="color: red;">{{$message}}</span><br>
-            @enderror
-            <button>Actualizar experiencia</button>
-        </form>
-    </main>
-    @endsection
+<header class="header">
+    @extends('layout.header')
+</header>
+@section('content')
+<main class="container">
+    <form action="{{route('exp.update', ['person_experience' => $experience->id])}}" method="post" class="experience-form">
+        @csrf
+        @method('PATCH')
+        <label for="company_experience" class="form-label">Empresa:</label><br>
+        <input type="text" id="company_experience" placeholder="Empresa" name="company_experience" value="{{ old('company_experience') ?? $experience->company_experience }}" class="form-input"><br>
+        @error('company_experience')
+        <span class="error-message">{{$message}}</span><br>
+        @enderror
+        <label for="months_experience" class="form-label">Meses de Experiencia:</label><br>
+        <input type="text" id="months_experience" name="months_experience" placeholder="Meses de experiencia" value="{{ old('months_experience') ?? $experience->months_experience }}" class="form-input">
+        @error('months_experience')
+        <span class="error-message">{{$message}}</span><br>
+        @enderror
+        <label for="functions" class="form-label">Funciones Desempeñadas:</label><br>
+        <textarea id="functions" name="functions" placeholder="Funciones desempeñadas" rows="5" class="form-textarea">{{ old('functions') ?? $experience->functions }}</textarea><br>
+        @error('functions')
+        <span class="error-message">{{$message}}</span><br>
+        @enderror
+        <button class="submit-button">Actualizar experiencia</button>
+    </form>
+</main>
+@endsection
 </body>
 </html>
-
