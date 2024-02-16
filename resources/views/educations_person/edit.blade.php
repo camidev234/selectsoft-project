ab862cd429a9f437c0bf2ec636deeeb5cd36d4cf
camidev234 <camilo222andresace@gmail.com>
2024-02-16 18:51:25 -0500

    Edit education styles

--- resources/views/educations_person/edit.blade.php
+++ resources/views/educations_person/edit.blade.php
@@ -3,43 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/createEducation.css')}}">
+    <link rel="stylesheet" href="{{asset('css/editEducation.css')}}">
     <title>Document</title>
 </head>
 <body>
 <header class="header">
-        @extends('layout.header')
-    </header>
-    @section('content')
-    <main class="container">
-        <form action="{{route('educations.update', ['education_person' => $education->id])}}" method="post">
-            @csrf
-            @method('PATCH')
-            <label for="school_name">Institucion educativa: </label><br>
-            <input type="text" placeholder="institucion" name="shcool_name" value="{{old('shcool_name')}} {{$education->shcool_name}}"><br>
-            @error('shcool_name')
-                <span style="color:red;">{{$message}}</span><br>
-            @enderror
-            <label for="obtained_title">TItulo obtenido: </label><br>
-            <input type="text" name="obtained_title" placeholder="Titulo obtenido" value="{{old('obtained_title')}} {{$education->obtained_title}}">
-            @error('obtained_title')
-                <span style="color:red;">{{$message}}</span><br>
-            @enderror
-            <label for="status_id">Estado: </label><br>
-            <select name="study_status_id" id="">
-                @foreach($statuses as $status)
-                    <option value="{{$status->id}}">{{$status->study_status}}</option>
-                @endforeach
-            </select><br>
-            <label for="studyLevel_id">Nivel de estudio</label><br>
-            <select name="study_level_id" id="">
-                @foreach($study_levels as $level)
-                    <option value="{{$level->id}}">{{$level->study_level}}</option>
-                @endforeach
-            </select><br>
-            <button>Actualizar</button>
-        </form>
-    </main>
-    @endsection
+    @extends('layout.header')
+</header>
+@section('content')
+<main class="container">
+    <form action="{{route('educations.update', ['education_person' => $education->id])}}" method="post" class="education-form">
+        @csrf
+        @method('PATCH')
+        <label for="school_name" class="form-label">Institucion educativa:</label><br>
+        <input type="text" id="school_name" placeholder="Institución" name="school_name" value="{{ old('school_name') ?? $education->school_name }}" class="form-input"><br>
+        @error('school_name')
+        <span class="error-message">{{$message}}</span><br>
+        @enderror
+        <label for="obtained_title" class="form-label">Título obtenido:</label><br>
+        <input type="text" id="obtained_title" placeholder="Título obtenido" name="obtained_title" value="{{ old('obtained_title') ?? $education->obtained_title }}" class="form-input">
+        @error('obtained_title')
+        <span class="error-message">{{$message}}</span><br>
+        @enderror
+        <label for="status_id" class="form-label">Estado:</label><br>
+        <select id="status_id" name="study_status_id" class="form-select">
+            @foreach($statuses as $status)
+            <option value="{{$status->id}}" {{ old('study_status_id') == $status->id || $education->study_status_id == $status->id ? 'selected' : '' }}>{{$status->study_status}}</option>
+            @endforeach
+        </select><br>
+        <label for="studyLevel_id" class="form-label">Nivel de estudio</label><br>
+        <select id="studyLevel_id" name="study_level_id" class="form-select">
+            @foreach($study_levels as $level)
+            <option value="{{$level->id}}" {{ old('study_level_id') == $level->id || $education->study_level_id == $level->id ? 'selected' : '' }}>{{$level->study_level}}</option>
+            @endforeach
+        </select><br>
+        <button class="submit-button">Actualizar</button>
+    </form>
+</main>
+@endsection
 </body>
 </html>
