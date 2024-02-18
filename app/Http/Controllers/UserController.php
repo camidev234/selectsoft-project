ee08d66041bd0386b4accd5f0ffe1c67f71c91b6
camidev234 <camilo222andresace@gmail.com>
2024-02-17 21:42:17 -0500

    conflict with routes solved

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -87,7 +87,7 @@ class UserController extends Controller
             $newCandidate->save();
         }
 
-        return view('/auth/login');
+        return redirect()->route('user.login');
     }
 
     public function updatePassword()
