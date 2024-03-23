056af172f4206e5e17d788e7ede59098d68c0ae0
camidev234 <camilo222andresace@gmail.com>
2024-03-23 18:08:37 -0500

    Requisition request updated

--- app/Http/Requests/RequisitionRequest.php
+++ app/Http/Requests/RequisitionRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 class RequisitionRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * Determina si el usuario está autorizado para realizar esta solicitud.
      */
     public function authorize(): bool
     {
@@ -15,7 +15,7 @@ class RequisitionRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * Obtiene las reglas de validación que se aplican a la solicitud.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
@@ -31,6 +31,43 @@ class RequisitionRequest extends FormRequest
             'candidate_description' => 'required|string|max:700',
             'candidate_talents' => 'required|string|max:700',
             'selection_criteria' => 'required|string|max:700',
+        ];
+    }
+
+    /**
+     * Obtiene los mensajes de error para las reglas de validación definidas.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'charge_id.required' => 'El campo ID de cargo es obligatorio.',
+            'charge_id.exists' => 'El ID de cargo seleccionado no es válido.',
+            'job_description.required' => 'El campo descripción del trabajo es obligatorio.',
+            'job_description.string' => 'El campo descripción del trabajo debe ser una cadena de caracteres.',
+            'job_description.max' => 'El campo descripción del trabajo no debe exceder los :max caracteres.',
+            'justification.required' => 'El campo justificación es obligatorio.',
+            'justification.string' => 'El campo justificación debe ser una cadena de caracteres.',
+            'justification.max' => 'El campo justificación no debe exceder los :max caracteres.',
+            'ideal_candidate.required' => 'El campo candidato ideal es obligatorio.',
+            'ideal_candidate.string' => 'El campo candidato ideal debe ser una cadena de caracteres.',
+            'ideal_candidate.max' => 'El campo candidato ideal no debe exceder los :max caracteres.',
+            'mission_charge.required' => 'El campo misión del cargo es obligatorio.',
+            'mission_charge.string' => 'El campo misión del cargo debe ser una cadena de caracteres.',
+            'mission_charge.max' => 'El campo misión del cargo no debe exceder los :max caracteres.',
+            'responsabilities.required' => 'El campo responsabilidades es obligatorio.',
+            'responsabilities.string' => 'El campo responsabilidades debe ser una cadena de caracteres.',
+            'responsabilities.max' => 'El campo responsabilidades no debe exceder los :max caracteres.',
+            'candidate_description.required' => 'El campo descripción del candidato es obligatorio.',
+            'candidate_description.string' => 'El campo descripción del candidato debe ser una cadena de caracteres.',
+            'candidate_description.max' => 'El campo descripción del candidato no debe exceder los :max caracteres.',
+            'candidate_talents.required' => 'El campo talentos del candidato es obligatorio.',
+            'candidate_talents.string' => 'El campo talentos del candidato debe ser una cadena de caracteres.',
+            'candidate_talents.max' => 'El campo talentos del candidato no debe exceder los :max caracteres.',
+            'selection_criteria.required' => 'El campo criterios de selección es obligatorio.',
+            'selection_criteria.string' => 'El campo criterios de selección debe ser una cadena de caracteres.',
+            'selection_criteria.max' => 'El campo criterios de selección no debe exceder los :max caracteres.',
         ];
     }
 }
--- resources/views/requisition/create.blade.php
+++ resources/views/requisition/create.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="{{asset('css/createReq.css')}}"> <!-- Enlazar archivo de estilos externo -->
+    <link rel="stylesheet" href="{{asset('css/createReq.css')}}">
 </head>
 
 <body>
@@ -14,7 +14,7 @@
     <form method="post" action="{{route('requisition.store', ['company' => $company->id])}}" class="form">
         @csrf
         <div class="form-group">
-            <label for="occupation_code">Cargo:</label>
+            <label for="occupation_code">Cargo <span class="ast">*</span></label>
             <select name="charge_id" id="charge_id" class="form-control">
                 @forelse($jobs as $job)
                 <option value="{{$job->id}}">{{$job->charge}}</option>
@@ -27,57 +27,57 @@
         <span>{{$message}}</span>
         @enderror
         <div class="form-group">
-            <label for="job_description">Descripcion del cargo:</label>
-            <textarea name="job_description" id="job_description" class="form-control" cols="30" rows="10" placeholder="Descripcion del cargo"></textarea>
+            <label for="job_description">Descripcion del cargo <span class="ast">*</span></label>
+            <textarea name="job_description" id="job_description" class="form-control" cols="30" rows="10" placeholder="Descripcion del cargo">{{old('job_description')}}</textarea>
         </div>
         @error('job_description')
         <span style="color: red;">{{$message}}</span><br><br>
         @enderror
         <div class="form-group">
-            <label for="justification">Justificacion:</label>
-            <textarea name="justification" id="justification" class="form-control" cols="30" rows="10" placeholder="Justificacion"></textarea>
+            <label for="justification">Justificacion <span class="ast">*</span></label>
+            <textarea name="justification" id="justification" class="form-control" cols="30" rows="10" placeholder="Justificacion">{{old('justification')}}</textarea>
         </div>
         @error('justification')
         <span style="color: red;">{{$message}}</span><br><br>
         @enderror
         <div class="form-group">
-            <label for="ideal_candidate">Candidato Ideal:</label>
-            <textarea name="ideal_candidate" id="ideal_candidate" class="form-control" cols="30" rows="10" placeholder="Candidato Ideal"></textarea>
+            <label for="ideal_candidate">Candidato Ideal <span class="ast">*</span></label>
+            <textarea name="ideal_candidate" id="ideal_candidate" class="form-control" cols="30" rows="10" placeholder="Candidato Ideal">{{old('ideal_candidate')}}</textarea>
         </div>
         @error('ideal_candidate')
         <span style="color: red;">{{$message}}</span><br><br>
         @enderror
         <div class="form-group">
-            <label for="mission_charge">Mision del cargo:</label>
-            <textarea name="mission_charge" id="mission_charge" class="form-control" cols="30" rows="10" placeholder="Mision del cargo"></textarea>
+            <label for="mission_charge">Mision del cargo <span class="ast">*</span></label>
+            <textarea name="mission_charge" id="mission_charge" class="form-control" cols="30" rows="10" placeholder="Mision del cargo">{{old('mission_charge')}}</textarea>
         </div>
         @error('mission_charge')
         <span style="color: red;">{{$message}}</span><br><br>
         @enderror
         <div class="form-group">
-            <label for="responsibilities">Responsabilidades:</label>
-            <textarea name="responsabilities" id="responsibilities" class="form-control" cols="30" rows="10" placeholder="Responsabilidades"></textarea>
+            <label for="responsibilities">Responsabilidades <span class="ast">*</span></label>
+            <textarea name="responsabilities" id="responsibilities" class="form-control" cols="30" rows="10" placeholder="Responsabilidades">{{old('responsabilities')}}</textarea>
         </div>
         @error('responsabilities')
         <span style="color: red;">{{$message}}</span><br><br>
         @enderror
         <div class="form-group">
-            <label for="candidate_description">Descripcion del candidato:</label>
-            <textarea name="candidate_description" id="candidate_description" class="form-control" cols="30" rows="10" placeholder="Descripcion del candidato"></textarea>
+            <label for="candidate_description">Descripcion del candidato <span class="ast">*</span></label>
+            <textarea name="candidate_description" id="candidate_description" class="form-control" cols="30" rows="10" placeholder="Descripcion del candidato">{{old('candidate_description')}}</textarea>
         </div>
         @error('candidate_description')
         <span style="color: red;">{{$message}}</span><br><br>
         @enderror
         <div class="form-group">
-            <label for="candidate_talents">Talentos del candidato:</label>
-            <textarea name="candidate_talents" id="candidate_talents" class="form-control" cols="30" rows="10" placeholder="Talentos del candidato"></textarea>
+            <label for="candidate_talents">Talentos del candidato <span class="ast">*</span></label>
+            <textarea name="candidate_talents" id="candidate_talents" class="form-control" cols="30" rows="10" placeholder="Talentos del candidato">{{old('candidate_talents')}}</textarea>
         </div>
         @error('candidate_talents')
         <span style="color: red;">{{$message}}</span><br><br>
         @enderror
         <div class="form-group">
-            <label for="selection_criteria">Criterios de seleccion:</label>
-            <textarea name="selection_criteria" id="selection_criteria" class="form-control" cols="30" rows="10" placeholder="Criterios de seleccion"></textarea>
+            <label for="selection_criteria">Criterios de seleccion <span class="ast">*</span></label>
+            <textarea name="selection_criteria" id="selection_criteria" class="form-control" cols="30" rows="10" placeholder="Criterios de seleccion">{{old('selection_criteria')}}</textarea>
         </div>
         @error('selection_criteria')
         <span style="color: red;">{{$message}}</span><br><br>
