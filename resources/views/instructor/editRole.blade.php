358d6f282337cbd0de739ebfa568f1dc7200c345
camidev234 <camilo222andresace@gmail.com>
2024-03-23 11:35:37 -0500

    Styles to edit role

--- resources/views/instructor/editRole.blade.php
+++ resources/views/instructor/editRole.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{asset('css/editRole.css')}}">
     <title>Editar Rol De Usuario</title>
 </head>
 
@@ -13,18 +13,21 @@
     @section('content')
     <section class="container">
         <section class="info">
-            <h4>Rol Actual: {{$userToMod->role->name}}</h4>
+            <h4 class="info__title">Usuario: {{ucwords(strtolower($userToMod->name))}} {{ucwords(strtolower($userToMod->last_name))}}-{{$userToMod->number_document}}</h4>
         </section>
-        <form action="{{route('instructor.updateRole', ['userMod' => $userToMod->id])}}" method="POST">
-            @csrf
+        <form action="{{route('instructor.updateRole', ['userMod' => $userToMod->id])}}" method="POST" class="form">
+        <article class="infotext">
+            <span>Rol Actual: {{$userToMod->role->name}}</span>
+        </article>    
+        @csrf
             @method('PATCH')
-            <select name="role_id" id="">
+            <select name="role_id" id="" class="form__select">
                 <option value="{{$userToMod->role_id}}">{{$userToMod->role->name}}</option>
                 @foreach($roles as $role)
                 <option value="{{$role->id}}">{{$role->name}}</option>
                 @endforeach
             </select><br>
-            <button>Actualizar Rol</button>
+            <button class="form__button">Actualizar Rol</button>
         </form>
     </section>
     @endsection
