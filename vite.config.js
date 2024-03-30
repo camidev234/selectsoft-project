b43c554e8f279b59685cd9e1ad6f5468c8a690b9
camidev234 <camilo222andresace@gmail.com>
2024-03-30 12:19:51 -0500

    Mod vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/css/', 'resources/js/app.js'],
+            input: ['resources/css/', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
