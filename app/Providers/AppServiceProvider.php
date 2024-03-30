a0cd52d14c16717766e15eb77945110124796403
camidev234 <camilo222andresace@gmail.com>
2024-03-30 14:31:27 -0500

    App env procvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
         PaginationPaginator::useBootstrap();
 
 
-        if (env('APP_ENV') !== 'local') {
+        if (env('APP_ENV') == 'local' && env('APP_DEBUG') == false) {
             URL::forceScheme('https');
         }
     }
