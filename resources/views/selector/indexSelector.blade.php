4a851ad2483d66d421535efefc677aee60ca0df2
melqui16rv <melquiveru@mail.com>
2024-04-01 14:27:14 -0500

    Another error solved 2

--- resources/views/selector/indexSelector.blade.php
+++ resources/views/selector/indexSelector.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/indexSelector.css')}}">
-    <title>Seleccionador Home</title>
+    <title>Seleccionador Inicio</title>
 </head>
 
 <body>
