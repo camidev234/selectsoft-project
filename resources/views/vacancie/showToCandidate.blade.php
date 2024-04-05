fb681b519730bdd449d5a0b665154cedefcc0e4c
camidev234 <camilo222andresace@gmail.com>
2024-04-04 23:29:59 -0500

    Updt vacancie show

--- resources/views/vacancie/showToCandidate.blade.php
+++ resources/views/vacancie/showToCandidate.blade.php
@@ -34,8 +34,8 @@
             </section>
             <section class="otherGeneralInfo">
                 <article class="firstGeneral">
-                    <p><strong>Experiencia requerida: </strong>{{$vacancie->required_experience}} meses</p>
-                    <p><strong>Numero de vacantes: </strong>{{$vacancie->number_vacancies}}</p>
+                    <p><strong>Experiencia requerida: </strong>{{$vacancie->requisiton->required_experience}} meses</p>
+                    <p><strong>Numero de vacantes: </strong>{{$vacancie->requisiton->number_vacancies}}</p>
                     <p><strong>Rango salarial: </strong>${{$vacancie->salary_range}}</p>
                     <p><strong>Empresa: </strong>{{$vacancie->company->business_name}}-{{$vacancie->company->nit}}</p>
                 </article>
